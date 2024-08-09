5058ec52e950e07c8d10119d36d6897f30efe1f3
Niall McElvogue <niallmcelvogue929@hotmail.co.uk>
2024-08-09 14:55:33 +0100

    initial commit

---
+++ src/main/java/com/example/smart_home_automation_system/SmartHomeAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_home_automation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeAutomationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeAutomationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_home_automation_system/SmartHomeAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_home_automation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeAutomationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
