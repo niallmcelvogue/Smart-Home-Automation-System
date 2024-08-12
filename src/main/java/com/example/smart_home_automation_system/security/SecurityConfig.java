d74e617f2e423b682328ce7dad3a381d67c4409b
Niall McElvogue <niallmcelvogue929@hotmail.co.uk>
2024-08-12 15:02:59 +0100

    Added user package and basic security

---
+++ src/main/java/com/example/smart_home_automation_system/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.smart_home_automation_system.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection using the updated method
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Allow all requests without authentication
+                );
+
+        return http.build();
+    }
+}
+
+
---
+++ src/main/java/com/example/smart_home_automation_system/user/UserController.java
@@ -0,0 +1,26 @@
+package com.example.smart_home_automation_system.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/addNewUser")
+    public User addNewUser(@RequestBody User user) {
+        userService.addNewUser(user);
+        return user;
+    }
+}
+
